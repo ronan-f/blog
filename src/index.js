61837c2e92f8f5d6d5a6d094c9ab88ce40f3e746
ronan-f <rfegan13@gmail.com>
2019-05-20 15:10:31 -0700

    Connecting redux thunk and setting up api call

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
         <App/>
     </Provider>,
     document.querySelector('#root')
